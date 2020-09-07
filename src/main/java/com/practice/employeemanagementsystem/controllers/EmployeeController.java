23eb222d08547b0251dc43e9e4b3a68e965da9ac
Diraj Thajali <thajalidiraj@gmail.com>
2020-09-07 14:35:05 -0700

    Created Employee Controller and added list employees functionality. Closes #5 and Closes #6.

---
+++ src/main/java/com/practice/employeemanagementsystem/controllers/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.practice.employeemanagementsystem.controllers;
+
+import com.practice.employeemanagementsystem.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getEmployees());
+        return "employees/list";
+    }
+}
