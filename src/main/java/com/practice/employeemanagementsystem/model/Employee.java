319fab4739a246dd635d19f696636515f8c34e36
Diraj Thajali <thajalidiraj@gmail.com>
2020-09-07 14:10:17 -0700

    Added Lombok annotations on Employee. Closes #1 #2.

--- src/main/java/com/practice/employeemanagementsystem/model/Employee.java
+++ src/main/java/com/practice/employeemanagementsystem/model/Employee.java
@@ -1,10 +1,27 @@
 package com.practice.employeemanagementsystem.model;
 
+import lombok.*;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "employees")
 public class Employee {
 
+    @Column(name = "first_name")
     public String firstName;
 
+    @Column(name = "last_name")
     public String lastName;
 
+    @Column(name = "email")
     public String email;
 }
