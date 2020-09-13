dfd8c6d09349a04c0b3eee9eb9c1fe4f8c460ca1
Diraj Thajali <thajalidiraj@gmail.com>
2020-09-13 00:16:25 -0700

    Added Login Controller. Closes #28.

---
+++ src/main/java/com/practice/employeemanagementsystem/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.practice.employeemanagementsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login/loginform";
+    }
+}
